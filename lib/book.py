579bda06dfc47bab945a312bb0d72fea5de136be
Ephy <macbookpro@Ephys-MacBookPro.local>
2025-12-02 08:24:16 +0300

    done

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        """Initialize a Book with a title and page_count."""
+        self.title = title
+        self._page_count = None
+        self.page_count = page_count
+    
+    @property
+    def page_count(self):
+        """Get the page_count."""
+        return self._page_count
+    
+    @page_count.setter
+    def page_count(self, value):
+        """Set the page_count with validation."""
+        if isinstance(value, int):
+            self._page_count = value
+        else:
+            print("page_count must be an integer")
+    
+    def turn_page(self):
+        """Output a message about turning the page."""
+        print("Flipping the page...wow, you read fast!")
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        """Initialize a Shoe with a brand and size."""
+        self.brand = brand
+        self._size = None
+        self.size = size
+        self.condition = "Used"
+    
+    @property
+    def size(self):
+        """Get the size."""
+        return self._size
+    
+    @size.setter
+    def size(self, value):
+        """Set the size with validation."""
+        if isinstance(value, int):
+            self._size = value
+        else:
+            print("size must be an integer")
+    
+    def cobble(self):
+        """Repair the shoe and set condition to 'New'."""
+        print("Your shoe is as good as new!")
+        self.condition = "New"
